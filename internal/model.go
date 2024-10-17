f821e45b9e4992894b9857be00361aa904113ae3
Ishi <chitrda112@gmail.com>
2024-10-18 05:08:27 +0700

    feat: add Task, TaskStatus

--- internal/model.go
+++ internal/model.go
@@ -1,1 +1,19 @@
 package internal
+
+import "time"
+
+type TaskStatus string
+
+const (
+	TODO        TaskStatus = "To Do"
+	IN_PROGRESS TaskStatus = "In-Progress"
+	DONE        TaskStatus = "Done"
+)
+
+type Task struct {
+	Id          int
+	Description string
+	Status      TaskStatus
+	CreatedAt   time.Time
+	UpdateAt 	time.Time
+}
