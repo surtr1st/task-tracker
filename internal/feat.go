6f2927f22cfdf3645b2a9b058509fcdca957046a
Ishi <chitrda112@gmail.com>
2024-10-18 05:46:11 +0700

    feat(feat)!: pre-add template

--- internal/feat.go
+++ internal/feat.go
@@ -1,1 +1,13 @@
 package internal
+
+type taskTracker struct{}
+
+func UseTaskTracker() taskTracker {
+	return taskTracker{}
+}
+
+func (tracker taskTracker) Add(description string) {}
+
+func (tracker taskTracker) Update() {}
+
+func (tracker taskTracker) List() {}
