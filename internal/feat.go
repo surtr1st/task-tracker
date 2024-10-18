99326c5a19da23f4a7950ea74471b9495d88e1db
Ishi <chitrda112@gmail.com>
2024-10-18 07:05:02 +0700

    feat(feat)!: partially implement update method

--- internal/feat.go
+++ internal/feat.go
@@ -1,13 +1,40 @@
 package internal
 
-type taskTracker struct{}
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+)
 
-func UseTaskTracker() taskTracker {
-	return taskTracker{}
+type taskTracker struct {
+	parser taskParser
+}
+
+func UseTaskTracker() *taskTracker {
+	return &taskTracker{
+		parser: UseTaskParser(),
+	}
 }
 
 func (tracker taskTracker) Add(description string) {}
 
-func (tracker taskTracker) Update() {}
+func (tracker taskTracker) Update(id int, value string) (string, error) {
+	tasks := tracker.parser.ToMap()
+	if task, ok := tasks[id]; ok {
+		task.Description = value
+		task.UpdatedAt = time.Now()
+		tasks[id] = task
+	}
+
+	updatedContent, err := json.Marshal(tasks)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := tracker.parser.Compose(updatedContent); err != nil {
+		log.Fatal(err)
+	}
+	return fmt.Sprintf("Updated task with id: %d", id), nil
+}
 
 func (tracker taskTracker) List() {}
