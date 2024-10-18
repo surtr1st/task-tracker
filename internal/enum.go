385e9dfacbb607b38f27ab82ecd361bbbc0437b9
Ishi <chitrda112@gmail.com>
2024-10-18 20:54:57 +0700

    feat: add enums

---
+++ internal/enum.go
@@ -0,0 +1,7 @@
+package internal
+
+const (
+	INVALID_INPUT     = "Your input ID is missing or not a number."
+	MISSING_FILE_PATH = "File path is empty!"
+	MISSING_CONTENT   = "Content is missing or invalid!"
+)
