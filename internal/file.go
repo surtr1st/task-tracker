799837df7e6dc9cf76c07cfbb372319188a5d7c8
Ishi <chitrda112@gmail.com>
2024-10-19 00:46:51 +0700

    feat(file)!: update logic for init data

--- internal/file.go
+++ internal/file.go
@@ -3,40 +3,41 @@ package internal
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 func InitData(targetFile string) error {
-	if _, err := os.OpenFile(targetFile, os.O_RDWR|os.O_EXCL, 0666); err != nil {
+	if _, err := os.OpenFile(targetFile, os.O_RDWR, 0666); err != nil {
 		if os.IsNotExist(err) {
 			var dir string
 			if IsWindows() {
-				dir = fmt.Sprintf("%s\\%s", UseEnv(WINDOWS), BASE_FOLDER)
+				dir = filepath.Join(UseEnv(WINDOWS), BASE_FOLDER)
 			} else {
-				dir = fmt.Sprintf("%s/.local/%s", UseEnv(LINUX), BASE_FOLDER)
+				dir = filepath.Join(UseEnv(LINUX), ".local", BASE_FOLDER)
 			}
 
-			if mkdirErr := os.Mkdir(dir, os.ModePerm); mkdirErr != nil {
-				if os.IsExist(err) {
-					f, createErr := os.Create(targetFile)
-					if createErr != nil {
-						return createErr
-					}
-					defer f.Close()
-
-					baseTemplate := `
-					{
-						"tasks": []
-					}
-					`
-					if writeErr := os.WriteFile(targetFile, []byte(baseTemplate), os.ModePerm); writeErr != nil {
-						return writeErr
-					}
-				}
-				return mkdirErr
+			if mkdirErr := os.MkdirAll(dir, os.ModePerm); mkdirErr != nil {
+				return fmt.Errorf("failed to create directory: %w", mkdirErr)
 			}
+
+			f, createErr := os.Create(targetFile)
+			if createErr != nil {
+				return fmt.Errorf("failed to create target file: %w", createErr)
+			}
+			defer f.Close()
+
+			baseTemplate := `{
+				"tasks": []
+			}`
+
+			if writeErr := os.WriteFile(targetFile, []byte(baseTemplate), os.ModePerm); writeErr != nil {
+				return fmt.Errorf("failed to write to target file: %w", writeErr)
+			}
+		} else {
+			return fmt.Errorf("failed to open target file: %w", err)
 		}
-
 	}
+
 	return nil
 }
 
