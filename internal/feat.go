02f69dca4c2876607e621c6f4e22e22ad290d3e4
Ishi <chitrda112@gmail.com>
2024-10-18 20:56:41 +0700

    feat(feat)!: adjust adding task

--- internal/feat.go
+++ internal/feat.go
@@ -3,7 +3,6 @@ package internal
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"time"
 )
 
@@ -21,8 +20,9 @@ func UseTaskTracker() *taskTracker {
 
 func (tracker taskTracker) Add(description string) string {
 	id := 1
-	totalTask := len(tracker.parser.Get().Tasks)
-	if totalTask > 1 {
+	tasks := tracker.parser.Get().Tasks
+	totalTask := len(tasks)
+	if totalTask >= 1 {
 		id = totalTask + 1
 	}
 
@@ -30,19 +30,16 @@ func (tracker taskTracker) Add(description string) string {
 		Id:          id,
 		Description: description,
 		Status:      TODO,
-		CreatedAt:   time.Now().String(),
-		UpdatedAt:   "",
+		CreatedAt:   FormatDate(time.Now()),
+		UpdatedAt:   "-",
 	}
-	tasks := TaskList{
-		Tasks: []Task{task},
-	}
-
-	value, err := json.MarshalIndent(tasks, "", " ")
+	tasks = append(tasks, task)
+	value, err := json.MarshalIndent(TaskList{Tasks: tasks}, "", " ")
 	if err != nil {
-		log.Fatal(err)
+		LogError(err.Error())
 	}
 	if err := tracker.parser.Compose(value); err != nil {
-		log.Fatal(err)
+		LogError(err.Error())
 	}
 
 	return fmt.Sprintf("Added new task! ID: %d", id)
@@ -50,10 +47,11 @@ func (tracker taskTracker) Add(description string) string {
 
 func (tracker taskTracker) Update(id int, value string) string {
 	tasks := tracker.parser.Get().Tasks
+
 	for i, task := range tasks {
 		if task.Id == id {
 			tasks[i].Description = value
-			tasks[i].UpdatedAt = time.Now().String()
+			tasks[i].UpdatedAt = FormatDate(time.Now())
 		}
 	}
 
@@ -61,13 +59,37 @@ func (tracker taskTracker) Update(id int, value string) string {
 		Tasks: tasks,
 	}, "", " ")
 	if err != nil {
-		log.Fatal(err)
+		LogError(err.Error())
 	}
 	if err := tracker.parser.Compose(updatedContent); err != nil {
-		log.Fatal(err)
+		LogError(err.Error())
 	}
 
 	return fmt.Sprintf("Updated task with ID: %d", id)
 }
 
-func (tracker taskTracker) List() {}
+func (tracker taskTracker) List() {
+	PrintTable(tracker.parser.Get().Tasks)
+}
+
+func (tracker taskTracker) Remove(id int) string {
+	tasks := tracker.parser.Get().Tasks
+
+	for i, task := range tasks {
+		if task.Id == id {
+			tasks = append(tasks[:i], tasks[i+1:]...)
+		}
+	}
+
+	updatedContent, err := json.MarshalIndent(TaskList{
+		Tasks: tasks,
+	}, "", " ")
+	if err != nil {
+		LogError(err.Error())
+	}
+	if err := tracker.parser.Compose(updatedContent); err != nil {
+		LogError(err.Error())
+	}
+
+	return fmt.Sprintf("Removed task with ID: %d", id)
+}
