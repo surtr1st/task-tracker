db7658f82653f8a8f374b21a2b3025d94e508326
Ishi <chitrda112@gmail.com>
2024-10-18 21:34:53 +0700

    feat(subcmd)!: handle update status + desc of task

--- internal/sub_command.go
+++ internal/sub_command.go
@@ -15,31 +15,41 @@ func ParseSubcommands() {
 		if err := InitData(Data()); err != nil {
 			LogError(err.Error())
 		}
+
 	case ADD:
 		description := args[1]
 		fmt.Println(task.Add(description))
+
 	case UPDATE:
 		id, err := strconv.Atoi(args[1])
 		if err != nil {
 			LogError(INVALID_INPUT)
 		}
-		description := args[2]
-		fmt.Println(task.Update(id, description))
+		switch args[2] {
+		case string(FLAG_IN_PROGRESS), string(FLAG_DONE), string(FLAG_TODO):
+			fmt.Println(task.Update(id, args[2], UPDATE_STATUS))
+		default:
+			fmt.Println(task.Update(id, args[2], UPDATE_DESCRIPTION))
+		}
+
 	case REMOVE:
 		id, err := strconv.Atoi(args[1])
 		if err != nil {
 			LogError(INVALID_INPUT)
 		}
 		fmt.Println(task.Remove(id))
+
 	case _DONE:
 		id := args[1]
 		fmt.Println(id)
+
 	case LIST:
 		if len(args) > 1 {
 			task.List(FilterFlagList(args[1]))
 		} else {
 			task.List(FLAG_NONE)
 		}
+
 	case _IN_PROGRESS:
 		id := args[1]
 		fmt.Println(id)
