11815f6f83acbd7209ce0c7c89038069597954a4
Ishi <chitrda112@gmail.com>
2024-10-18 20:55:38 +0700

    feat(sub_cmd)!: attach features to subcommands

--- internal/sub_command.go
+++ internal/sub_command.go
@@ -3,6 +3,7 @@ package internal
 import (
 	"flag"
 	"fmt"
+	"strconv"
 )
 
 const (
@@ -12,36 +13,52 @@ const (
 	LIST            = "list"
 	SET_DONE        = "done"
 	SET_IN_PROGRESS = "ip"
+	INIT            = "init"
 )
 
 func ParseSubcommands() {
+	task := UseTaskTracker()
 	flag.Parse()
 	args := flag.Args()
 	switch args[0] {
+	case INIT:
+		if err := InitData(Data()); err != nil {
+			LogError(err.Error())
+		}
+	case ADD:
+		description := args[1]
+		fmt.Println(task.Add(description))
 	case UPDATE:
-		id := args[1]
-		newDescription := args[2]
-		fmt.Println(id, newDescription)
+		id, err := strconv.Atoi(args[1])
+		if err != nil {
+			LogError(INVALID_INPUT)
+		}
+		description := args[2]
+		fmt.Println(task.Update(id, description))
 	case REMOVE:
-		id := args[1]
-		fmt.Println(id)
+		id, err := strconv.Atoi(args[1])
+		if err != nil {
+			LogError(INVALID_INPUT)
+		}
+		fmt.Println(task.Remove(id))
 	case SET_DONE:
 		id := args[1]
 		fmt.Println(id)
 	case LIST:
-		if args[1] == "--" {
-			switch args[2] {
-			case SET_DONE:
-				break
-			default:
-				break
+		if len(args) > 1 {
+			if args[1] == "--" {
+				switch args[2] {
+				case SET_DONE:
+					break
+				case SET_IN_PROGRESS:
+					break
+				}
 			}
+		} else {
+			task.List()
 		}
 	case SET_IN_PROGRESS:
 		id := args[1]
 		fmt.Println(id)
-	default:
-		description := args[1]
-		fmt.Println(description)
 	}
 }
